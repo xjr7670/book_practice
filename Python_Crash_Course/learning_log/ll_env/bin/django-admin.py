0a9e1dbe95147c05e562c17f4298142144b8c597
Cavin <cavin@localhost.localdomain>
2017-09-02 15:06:09 +0800

    第三个项目：Django入门

---
+++ Python_Crash_Course/learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cavin/Code/Python/Python_Crash_Course/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
