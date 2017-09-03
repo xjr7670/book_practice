19cd94f9fa66b7e33f7646c0c0ef4c262d563247
Cavin <xjr30226@126.com>
2017-09-03 11:30:48 +0800

    增加登录要求

--- Python_Crash_Course/learning_log/learning_logs/views.py
+++ Python_Crash_Course/learning_log/learning_logs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -8,19 +11,30 @@ def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
 
+def check_topic_owner(owner, user):
+    """用于检查当前用户"""
+    if owner != user:
+        raise Http404
+
+@login_required
 def topics(request):
     """显示所有的主题"""
-    topics = Topic.objects.order_by('date_added')
+
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有的条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求的主题属于当前用户
+    check_topic_owner(topic.owner, request.user)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """添加新主题"""
 
@@ -31,16 +45,20 @@ def new_topic(request):
         # POST提交的数据，对数据进行处理
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('learnings_logs:topics'))
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """在特定的主题中添加新条目"""
 
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic.owner, request.user)
 
     if request.method != "POST":
         # 未提交数据，创建一个空表单
@@ -56,11 +74,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """编辑既有条目"""
 
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(topic.owner, request.user)
 
     if request.method != "POST":
         # 初次请求，使用当前条目填充表单
