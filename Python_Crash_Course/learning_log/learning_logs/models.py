b58da7791498a6390b710173c9f4ebceeace3e10
Cavin <xjr30226@126.com>
2017-09-03 11:29:46 +0800

    增加主题所有者

--- Python_Crash_Course/learning_log/learning_logs/models.py
+++ Python_Crash_Course/learning_log/learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """用户学习的主题"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
 
     def __str__(self):
         """返回模型的字符串表示"""
