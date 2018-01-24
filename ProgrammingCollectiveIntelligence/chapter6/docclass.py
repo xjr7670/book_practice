ba92f3ca12f06c16bfaeb5d318ec541582f3e51c
Cavin <xjr30226@126.com>
2018-01-24 22:48:25 +0800

    第6章 朴素贝叶斯方法

--- ProgrammingCollectiveIntelligence/chapter6/docclass.py
+++ ProgrammingCollectiveIntelligence/chapter6/docclass.py
@@ -10,10 +10,17 @@ import re
 import math
 
 
+def sampletrain(cl):
+    cl.train('Nobody owns the water', 'good')
+    cl.train('the quick rabbit jumps fences', 'good')
+    cl.train('buy pharmaceuticals now', 'bad')
+    cl.train('make quick money at the online casino', 'bad')
+    cl.train('the quick brown fox jumps', 'good')
+
 def getwords(doc):
     splitter = re.compile('\\W*')
     # 根据非字母字符进行单词拆分
-    words = [s.lower() for x in splitter.splic(doc)
+    words = [s.lower() for s in splitter.split(doc)
              if len(s) > 2 and len(s) < 20]
     
     # 只返回一组不重复的单词
@@ -35,7 +42,7 @@ class classifier:
     
     # 增加对某一分类的计数值
     def incc(self, cat):
-        self.cc.setdefault(cat, )
+        self.cc.setdefault(cat, 0)
         self.cc[cat] += 1
     
     # 某一特征出现于某一分类中的次数
@@ -67,3 +74,77 @@ class classifier:
         
         # 增加针对该分类的计数值
         self.incc(cat)
+
+
+    def fprob(self, f, cat):
+        if self.catcount(cat) == 0:
+            return 0
+
+        # 特征在分类中出现的总次数，除以分类中包含内容项的总数
+        return self.fcount(f, cat) / self.catcount(cat)
+
+    
+    # 计算加权平均后的假设概率
+    def weightedprob(self, f, cat, prf, weight=1.0, ap=0.5):
+        # 计算当前的概率值
+        basicprob = prf(f, cat)
+
+        # 统计特征在所有分类中出现的次数
+        totals = sum([self.fcount(f, c) for c in self.categories()])
+
+        # 计算加权平均
+        bp = ((weight * ap) + (totals * basicprob)) / (weight + totals)
+        return bp
+
+
+    def classify(self, item, default=None):
+        probs = {}
+        # 寻找概率最大的分类
+        max = 0.0
+        for cat in self.categories():
+            probs[cat] = self.prob(item, cat)
+            if probs[cat] > max:
+                max = probs[cat]
+                best = cat
+
+        # 确保概率值超出域值*次大概率值
+        for cat in probs:
+            if cat == best:
+                continue
+            if probs[cat] * self.getthreshold(best) > probs[best]:
+                return default
+
+        return best
+
+
+class naivebayes(classifier):
+
+    def __init__(self, getfeatures):
+        classifier.__init__(self, getfeatures)
+        self.thresholds = {}
+
+
+    def setthreshold(self, cat, t):
+        self.thresholds[cat] = t
+
+
+    def getthreshold(self, cat):
+        if cat not in self.thresholds:
+            return 1.0
+        return self.thresholds[cat]
+
+
+    def docprob(self, item, cat):
+        features = self.getfeatures(item)
+
+        # 将所有特征的概率相乘
+        p = 1
+        for f in features:
+            p *= self.weightedprob(f, cat, self.fprob)
+
+        return p
+
+    def prob(self, item, cat):
+        catprob = self.catcount(cat) / self.totalcount()
+        docprob = self.docprob(item, cat)
+        return docprob * catprob
