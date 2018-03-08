0a06f19f7c766d5c65b9843208deec6abb27772d
Cavin <xjr30226@126.com>
2018-03-08 22:49:57 +0800

    第4章，朴素贝叶斯分类

---
+++ MachineLearninginAction/ch4/bayes.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import numpy as np
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+            ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+            ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+            ['stop', 'posting', 'stupid', 'worthless', 'garbages'],
+            ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+            ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    p0Num = np.zeros(numWords)
+    p1Num = np.zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            # 因为这里的trainMatrix已经是被转换为数字向量了
+            # 这里的累加，加的是属于1这个类别的样本中的各个词对应的次数加1
+            p1Num += trainMatrix[i]
+            # 这里的累加，就是把出现于该类别中的词的次数累加起来，是类别1中的所有词的出现次数
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 最后再除，就得到这个类别中的每个词，在每个类别中的出现频率了
+    p1Vect = p1Num / p1Denom
+    p0Vect = p0Num / p0Denom
+    return p0Vect, p1Vect, pAbusive
