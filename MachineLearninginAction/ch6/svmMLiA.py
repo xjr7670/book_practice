63e60e4e9523a55a367630dc34ab3ff535898560
Cavin <xjr30226@126.com>
2018-03-18 11:43:15 +0800

    完善SVM模型

--- MachineLearninginAction/ch6/svmMLiA.py
+++ MachineLearninginAction/ch6/svmMLiA.py
@@ -96,7 +96,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 
 
 class optStruct:
-    def __init__(self, dataMatIn, classLabels, C, toler):
+    def __init__(self, dataMatIn, classLabels, C, toler, kTup):
         self.X = dataMatIn
         self.labelMat = classLabels
         self.C = C
@@ -105,10 +105,12 @@ class optStruct:
         self.alphas = np.mat(np.zeros((self.m, 1)))
         self.b = 0
         self.eCache = np.mat(np.zeros((self.m, 2)))
+        self.K = np.mat(np.zeros((self.m, self.m)))
+        for i in range(self.m):
+            self.K[:, i] = kernelTrans(self.X, self.X[i, :], kTup)
 
 def calcEk(oS, k):
-    fXk = float(np.multiply(oS.alphas, oS.labelMat).T * \
-            (oS.X * oS.X[k, :].T)) + oS.b
+    fXk = float(np.multiply(oS.alphas, oS.labelMat).T * oS.K[:, k] + oS.b)
     Ek = fXk - float(oS.labelMat[k])
     return Ek
 
@@ -141,8 +143,8 @@ def updateEk(oS, k):
 
 def innerL(i, oS):
     Ei = calcEk(oS, i)
-    if ((os.labelMat[i] * Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or \
-            ((os.labelMat[i] * Ei > oS.tol) and (oS.alphas[i] > 0)):
+    if ((oS.labelMat[i] * Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or \
+            ((oS.labelMat[i] * Ei > oS.tol) and (oS.alphas[i] > 0)):
         j, Ej = selectJ(i, oS, Ei)
         alphaIold = oS.alphas[i].copy()
         alphaJold = oS.alphas[j].copy()
@@ -155,13 +157,12 @@ def innerL(i, oS):
         if L == H:
             print("L == H")
             return 0
-        eta = 2.0 * oS.X[i, :] * oS.X[j, :].T - oS.X[i, :] * oS.X[i, :].T - \
-                oS.X[j, :] * oS.X[j, :].T
+        eta = 2.0 * oS.K[i, j] - oS.K[i, i] - oS.K[j, j]
         if eta >= 0:
             print("eta >= 0")
             return 0
         oS.alphas[j] -= oS.labelMat[j] * (Ei - Ej) / eta
-        os.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
         updateEk(oS, j)
         if (abs(oS.alphas[j] - alphaJold) < 0.00001):
             print("j not moving enough")
@@ -170,11 +171,9 @@ def innerL(i, oS):
                 (alphaJold - oS.alphas[j])
         updateEk(oS, i)
         b1 = oS.b - Ei - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * \
-                oS.X[i, :] * oS.X[i, :].T - oS.labelMat[j] * \
-                (oS.alphas[j] - alphaJold) * oS.X[i, :] * oS.X[j, :].T
+                oS.K[i, i] - oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[i, j]
         b2 = oS.b - Ej - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * \
-                oS.X[i, :] * oS.X[j, :].T - oS.labelMat[j] * \
-                (oS.alphas[j] - alphaJold) * oS.X[j, :] * oS.X[j, :].T
+                oS.K[i, j] - oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[j, j]
         if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]):
             oS.b = b1
         elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]):
@@ -184,3 +183,85 @@ def innerL(i, oS):
         return 1
     else:
         return 0
+
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+    oS = optStruct(np.mat(dataMatIn), np.mat(classLabels).transpose(), C, toler, kTup)
+    _iter = 0
+    entireSet = True
+    alphaPairsChanged = 0
+    while (_iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+        alphaPairsChanged = 0
+        if entireSet:
+            for i in range(oS.m):
+                alphaPairsChanged += innerL(i, oS)
+                print("fullSet, iter: %d i: %d, pairs changed %d" % \
+                        (_iter, i, alphaPairsChanged))
+            _iter += 1
+        else:
+            nonBoundIs = np.nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i, oS)
+                print("non-bound, iter: %d i:%d, pairs changed %d" % \
+                        (_iter, i, alphaPairsChanged))
+            _iter += 1
+        if entireSet:
+            entireSet = False
+        elif (alphaPairsChanged == 0):
+            entireSet = True
+        print("iteration number: %d" % _iter)
+    return oS.b, oS.alphas
+
+def calcWs(alphas, dataArr, classLabels):
+    X = np.mat(dataArr)
+    labelMat = np.mat(classLabels).transpose()
+    m, n = np.shape(X)
+    w = np.zeros((n, 1))
+    for i in range(m):
+        w += np.multiply(alphas[i] * labelMat[i], X[i, :].T)
+    return w
+
+
+def kernelTrans(X, A, kTup):
+    m, n = np.shape(X)
+    K = np.mat(np.zeros((m, 1)))
+    if kTup[0] == 'lin':
+        K = X * A.T
+    elif kTup[0] == 'rbf':
+        for j in range(m):
+            deltaRow = X[j, :] - A
+            K[j] = deltaRow * deltaRow.T
+        K = np.exp(K / (-1 * kTup[1] ** 2))
+    else:
+        raise NameError('Houston We Have a Problem -- \
+                That Kernel is not recognized')
+    return K
+
+
+def testRbf(k1=1.3):
+    dataArr, labelArr = loadDataSet('testSetRBF.txt')
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1))
+    dataMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    svInd = np.nonzero(alphas.A > 0)[0]
+    sVs = dataMat[svInd]
+    labelSV = labelMat[svInd]
+    print("there are %d Support Vectors" % np.shape(sVs)[0])
+    m, n = np.shape(dataMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, dataMat[i, :], ('rbf', k1))
+        predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+        if np.sign(predict) != np.sign(labelArr[i]):
+            errorCount += 1
+    print("the traninig error rate is: %f" % (float(errorCount) / m))
+    dataArr, labelArr = loadDataSet('testSetRBF2.txt')
+    errorCount = 0
+    dataMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    m, n = np.shape(dataMat)
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, dataMat[i, :], ('rbf', k1))
+        predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+        if np.sign(predict) != np.sign(labelArr[i]):
+            errorCount += 1
+    print("the test error rate is: %f" % (float(errorCount) / m))
