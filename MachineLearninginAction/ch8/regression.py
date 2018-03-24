6c73899fe9ee64f47574d5e1df6c6ad487fd1488
Cavin <xjr30226@126.com>
2018-03-24 11:03:23 +0800

    第8章 回归

--- MachineLearninginAction/ch8/regression.py
+++ MachineLearninginAction/ch8/regression.py
@@ -26,3 +26,93 @@ def standRegres(xArr, yArr):
         return
     ws = xTx.I * (xMat.T * yMat)
     return ws
+
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    m = np.shape(xMat)[0]
+    weights = np.mat(np.eye((m)))
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = np.exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+    xTx = xMat.T * (weights * xMat)
+    if np.linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = np.shape(testArr)[0]
+    yHat = np.zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
+def rssError(yArr, yHatArr):
+    return ((yArr - yHatArr) ** 2).sum()
+
+
+# 岭回归
+def ridgeRegres(xMat, yMat, lam=0.2):
+    xTx = xMat.T * xMat
+    denom = xTx + np.eye(np.shape(xMat)[1]) * lam
+    if np.linalg.det(denom) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = denom.I * (xMat.T * yMat)
+    return ws
+
+def ridgeTest(xArr, yArr):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    yMean = np.mean(yMat, 0)
+    yMat = yMat - yMean
+    xMeans = np.mean(xMat, 0)
+    xVar = np.var(xMat, 0)
+    xMat = (xMat - xMeans) / xVar
+    numTestPts = 30
+    wMat = np.zeros((numTestPts, np.shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegres(xMat, yMat, np.exp(i-10))
+        wMat[i, :] = ws.T
+    return wMat
+
+
+# 标准化函数
+# 用于把矩阵标准化为均值为0、方差为1的矩阵
+def regularize(xMat):
+    inMat = xMat.copy()
+    inMeans = np.mean(inMat, 0)
+    inVar = np.var(inMat, 0)
+    inMat = (inMat - inMeans) / inVar
+    return inMat
+
+# 前向逐步线性回归
+def stageWist(xArr, yArr, eps=0.01, numIt=100):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    yMean = np.mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m, n = np.shape(xMat)
+    returnMat = np.zeros((numIt, n))
+    ws = np.zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+    for i in range(numIt):
+        print(ws.T)
+        lowestError = np.inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign
+                yTest = xMat * wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i, :] = ws.T
+    return returnMat
