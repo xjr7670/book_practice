435f9f7837ac830b6b4fd061e316283dbbae707d
Cavin <xjr30226@126.com>
2018-03-31 15:20:05 +0800

    第10章 K均值聚类

--- MachineLearninginAction/ch10/kMeans.py
+++ MachineLearninginAction/ch10/kMeans.py
@@ -49,8 +49,110 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
             if clusterAssment[i, 0] != minIndex:
                 clusterChanged = True
             clusterAssment[i, :] = minIndex, minDist ** 2
-        print(centroids)
+        #print(centroids)
         for cent in range(k):
             ptsInClust = dataSet[np.nonzero(clusterAssment[:, 0] == cent)[0]]
             centroids[cent, :] = np.mean(ptsInClust, axis=0)
     return centroids, clusterAssment
+
+
+# 二分K-均值聚类算法
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    centroid0 = np.mean(dataSet, axis=0).tolist()[0]
+    centList = [centroid0]
+    for j in range(m):
+        clusterAssment[j, 1] = distMeas(np.mat(centroid0), dataSet[j, :])**2
+    while (len(centList) < k):
+        lowestSSE = np.inf
+        for i in range(len(centList)):
+            ptsInCurrCluster = dataSet[np.nonzero(clusterAssment[:, 0].A == i)[0], :]
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = np.sum(splitClustAss[:, 1])
+            sseNotSplit = np.sum(clusterAssment[np.nonzero(clusterAssment[:, 0].A != i)[0], 1])
+            print("sseSplit, and notSplit: ", sseSplit, sseNotSplit)
+            if (sseSplit + sseNotSplit) < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        bestClustAss[np.nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
+        bestClustAss[np.nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+        print("the bestCentToSplit is: ", bestCentToSplit)
+        print("the len of bestClustAss is: ", len(bestClustAss))
+        centList[bestCentToSplit] = bestNewCents[0, :].tolist()[0]
+        centList.append(bestNewCents[1, :].tolist()[0])
+        clusterAssment[np.nonzero(clusterAssment[:, 0].A == bestCentToSplit)[0], :] = bestClustAss
+    return np.mat(centList), clusterAssment
+
+
+# Yahoo!PlaceFinder API
+from urllib import parse
+from urllib import request
+import json
+def geoGrab(stAddress, city):
+    apiStem = 'http://where.yahooapis.com/geocode?'
+    params = {}
+    params['flags'] = 'J'
+    params['appid'] = 'ppp68N8t'
+    params['location'] = '%s %s' % (stAddress, city)
+    url_params = parse.urlencode(params)
+    yahooApi = apiStem + url_params
+    print(yahooApi)
+    c = request.urlopen(yahooApi)
+    return json.loads(c.read())
+
+from time import sleep
+def massPlaceFind(fileName):
+    fw = open('places.txt', 'w')
+    for line in open(fileName).readlines():
+        line = line.strip()
+        lineArr = line.split('\t')
+        retDict = getGrab(lineArr[1], lineArr[2])
+        if reDict['ResultSet']['Error'] == 0:
+            lat = float(retDict['ResultSet']['Results'][0]['latitude'])
+            lng = float(retDict['ResultSet']['Results'][0]['longitude'])
+            print("%s\t%f\t%f" % (lineArr[0], lat, lng))
+            fw.write('%s\t%f\t%f\n' % (line, lat, lng))
+        else:
+            print("error fetching")
+        sleep(1)
+    fw.close()
+
+
+# 球面距离计算及簇绘图函数
+def distSLC(vecA, vecB):
+    a = np.sin(vecA[0, 1] * np.pi / 180) * np.sin(vecB[0, 1] * np.pi / 180)
+    b = np.cos(vecA[0, 1] * np.pi / 180) * np.cos(vecB[0, 1] * np.pi / 180) *\
+            np.cos(np.pi * (vecB[0, 0] - vecA[0, 0]) / 180)
+    return np.arccos(a + b) * 6371.0
+
+import matplotlib
+import matplotlib.pyplot as plt
+def clusterClubs(numClust=5):
+    datList = []
+    for line in open('places.txt').readlines():
+        lineArr = line.split('\t')
+        datList.append([float(lineArr[4]), float(lineArr[3])])
+    datMat = np.mat(datList)
+    myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
+    fig = plt.figure()
+    rect = [0.1, 0.1, 0.8, 0.8]
+    scatterMarkers = ['s', 'o', '^', '8', 'p',
+                      'd', 'v', 'h', '>', '<']
+    axprops = dict(xticks=[], yticks=[])
+    ax0 = fig.add_axes(rect, label='ax0', **axprops)
+    imgP = plt.imread('Portland.png')
+    ax0.imshow(imgP)
+    ax1 = fig.add_axes(rect, label='ax1', frameon=False)
+    for i in range(numClust):
+        ptsInCurrCluster = datMat[np.nonzero(clustAssing[:, 0].A == i)[0], :]
+        markerStyle = scatterMarkers[i % len(scatterMarkers)]
+        ax1.scatter(ptsInCurrCluster[:, 0].flatten().A[0], 
+                ptsInCurrCluster[:, 1].flatten().A[0],
+                marker=markerStyle, s=90)
+    ax1.scatter(myCentroids[:, 0].flatten().A[0],
+            myCentroids[:, 1].flatten().A[0], marker='+', s=30)
+    plt.show()
+
