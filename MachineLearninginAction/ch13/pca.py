441149940891ffee02e43f92e150636dbaaf301b
Cavin <xjr30226@126.com>
2018-04-11 22:12:16 +0800

    第13章 因子降维

---
+++ MachineLearninginAction/ch13/pca.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import numpy as np
+
+def loadDataSet(fileName, delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
+    datArr = [map(float, line) for line in stringArr]
+    fr.close()
+    return np.mat(datArr)
+
+def pca(dataMat, topNfeat=9999999):
+    meanVals = np.mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals
+    covMat = np.cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = np.linalg.eig(np.mat(covMat))
+    eigValInd = np.argsort(eigVals)
+    eigValInd = eigValInd[:-(topNfeat+1):-1]
+    redEigVects = eigVects[:, eigValInd]
+    lowDDataMat = meanRemoved * redEigVects
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat, reconMat
