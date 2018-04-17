4d950afc94dc855f25eb4d694a2a4d2171b49729
Cavin <xjr30226@126.com>
2018-04-17 21:40:02 +0800

    第14章 奇异值分解降维

--- MachineLearninginAction/ch14/svdRec.py
+++ MachineLearninginAction/ch14/svdRec.py
@@ -4,15 +4,27 @@
 import numpy as np
 
 def loadExData():
-    return [[1, 1, 1, 0, 0],
-            [2, 2, 2, 0, 0],
-            [1, 1, 1, 0, 0],
-            [5, 5, 5, 0, 0],
-            [1, 1, 0, 2, 2],
+    return [[0, 0, 0, 2, 2],
             [0, 0, 0, 3, 3],
-            [0, 0, 0, 1, 1]]
+            [0, 0, 0, 1, 1],
+            [1, 1, 1, 0, 0],
+            [2, 2, 2, 0, 0],
+            [5, 5, 5, 0, 0],
+            [1, 1, 1, 0, 0]]
 
-
+def loadExData2():
+    return[[0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 5],
+           [0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 3],
+           [0, 0, 0, 0, 4, 0, 0, 1, 0, 4, 0],
+           [3, 3, 4, 0, 0, 0, 0, 2, 2, 0, 0],
+           [5, 4, 5, 0, 0, 0, 0, 5, 5, 0, 0],
+           [0, 0, 0, 0, 5, 0, 1, 0, 0, 5, 0],
+           [4, 3, 4, 0, 0, 0, 0, 5, 5, 0, 1],
+           [0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 4],
+           [0, 0, 0, 2, 0, 2, 5, 0, 0, 1, 2],
+           [0, 0, 0, 0, 5, 0, 0, 0, 0, 4, 0],
+           [1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0]]
+    
 # 相似度计算
 def ecludSim(inA, inB):
     """欧几里德距离"""
@@ -29,3 +41,87 @@ def cosSim(inA, inB):
     num = float(inA.T * inB)
     denom = np.linalg.norm(inA) * np.linalg.norm(inB)
     return 0.5 + 0.5 * (num / denom)
+
+# 基于物品相似度的推荐引擎
+def standEst(dataMat, user, simMeas, item):
+    n = np.shape(dataMat)[1]
+    simTotal = 0.0
+    ratSimTotal = 0.0
+    print(simMeas.__name__)
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0:
+            continue
+        overLap = np.nonzero(np.logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]
+        if len(overLap) == 0:
+            similarity = 0
+        else:
+            similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
+        print('the %d and %d similarity is: %f' % (item, j, similarity))
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0:
+        return 0
+    else:
+        return ratSimTotal / simTotal
+
+def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
+    unratedItems = np.nonzero(dataMat[user, :].A == 0)[1]
+    if len(unratedItems) == 0:
+        return 'you rated everything'
+    itemScores = []
+    for item in unratedItems:
+        estimatedScore = estMethod(dataMat, user, simMeas, item)
+        itemScores.append((item, estimatedScore))
+    return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[:N]
+
+# 基于SVD的评分估计
+def svdEst(dataMat, user, simMeas, item):
+    n = np.shape(dataMat)[1]
+    simTotal = 0.0
+    ratSimTotal = 0.0
+    U,Sigma,VT = np.linalg.svd(dataMat)
+    Sig4 = np.mat(np.eye(4) * Sigma[:4]) #arrange Sig4 into a diagonal matrix
+    xformedItems = dataMat.T * U[:,:4] * Sig4.I  #create transformed items
+    for j in range(n):
+        userRating = dataMat[user,j]
+        if userRating == 0 or j==item:
+            continue
+        similarity = simMeas(xformedItems[item,:].T,\
+                             xformedItems[j,:].T)
+        print('the %d and %d similarity is: %f' % (item, j, similarity))
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0:
+        return 0
+    else:
+        return ratSimTotal / simTotal
+
+# 图像压缩函数
+def printMat(inMat, thresh=0.8):
+    for i in range(32):
+        for k in range(32):
+            if float(inMat[i, k]) > thresh:
+                print(1, end='')
+            else:
+                print(0, end='')
+        print('')
+
+def imgCompress(numSV=3, thresh=0.8):
+    myl = []
+    for line in open('0_5.txt').readlines():
+        newRow = []
+        for i in range(32):
+            newRow.append(int(line[i]))
+        myl.append(newRow)
+    myMat = np.mat(myl)
+    print("****original matrix*****")
+    printMat(myMat, thresh)
+    U, Sigma, VT = np.linalg.svd(myMat)
+    SigRecon = np.mat(np.zeros((numSV, numSV)))
+    for k in range(numSV):
+        SigRecon[k, k] = Sigma[k]
+    reconMat = U[:, :numSV] * SigRecon * VT[:numSV, :]
+    print("****reconstructed matrix using %d singular values******" % numSV)
+    printMat(reconMat, thresh)
+        
