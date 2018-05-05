425a092dfe24d04d646ed4027d0a6a03c96c2481
Cavin <xjr30226@126.com>
2018-05-05 21:52:01 +0800

    优化算法

--- ProgrammingCollectiveIntelligence/chapter5/optimization.py
+++ ProgrammingCollectiveIntelligence/chapter5/optimization.py
@@ -160,3 +160,57 @@ def annealingoptimize(domain, costf, T=10000.0, cool=0.95, step=1):
         # 降低温度
         T = T * cool
     return vec
+
+
+# 遗传算法
+def geneticoptimize(domain, costf, popsize=50, step=1, mutprob=0.2, elite=0.2, maxiter=100):
+    # 变异操作
+    # 随机地把其中一个值进行增加或减少
+    def mutate(vec):
+        i = random.randint(0, len(domain)-1)
+        if random.random() < 0.5 and vec[i] > domain[i][0]:
+            return vec[0:i] + [vec[i]-step] + vec[i+1:]
+        elif vec[i] < domain[i][1]:
+            return vec[0:i] + [vec[i]+step] + vec[i+1:]
+
+    # 交叉操作
+    # 前面取r1的，后面取r2的
+    def crossover(r1, r2):
+        i = random.randint(1, len(domain)-2)
+        return r1[0:i] + r2[i:]
+
+    # 构造初始种群
+    pop = []
+    for i in range(popsize):
+        vec = [random.randint(domain[i][0], domain[i][1])
+                for i in range(len(domain))]
+        pop.append(vec)
+
+    # 每一代中有多少胜出者?
+    topelite = int(elite * popsize)
+
+    # 主循环
+    for i in range(maxiter):
+        scores = [(costf(v), v) for v in pop]
+        # 以下两行，把题解排了个序
+        scores.sort()
+        ranked = [v for (s, v) in scores]
+
+        # 从纯粹的胜出者开始
+        pop = ranked[0:topelite]
+
+        # 添加变异和配对后的胜出者
+        while len(pop) < popsize:
+            if random.random() < mutprob:
+                # 变异
+                c = random.randint(0, topelite)
+                pop.append(mutate(ranked[c]))
+            else:
+                # 交叉
+                c1 = random.randint(0, topelite)
+                c2 = random.randint(0, topelite)
+                pop.append(crossover(ranked[c1], ranked[c2]))
+
+        # 打印当前最优值
+        print(scores[0][0])
+    return scores[0][1]
