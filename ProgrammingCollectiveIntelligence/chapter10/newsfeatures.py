3e5955e95c8d9f986fc253505af05b6adea9841d
Cavin <xjr30226@126.com>
2018-05-19 15:35:35 +0800

    寻找独立特征

---
+++ ProgrammingCollectiveIntelligence/chapter10/newsfeatures.py
@@ -0,0 +1,12 @@
+#-*- coding:utf-8 -*-
+
+import feedparser
+import re
+
+
+feedlist=[
+          'http://www.foxnews.com/xmlfeed/rss/0,4313,0,00.rss',
+          'http://www.foxnews.com/xmlfeed/rss/0,4313,80,00.rss',
+          'http://www.foxnews.com/xmlfeed/rss/0,4313,81,00.rss',
+          ]
+
