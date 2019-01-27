a0dae1d0dacc8648e5587e905c26b83a5145aacd
xjr7670 <xjr30226@126.com>
2019-01-27 10:15:21 +0800

    chater5 Queue

---
+++ data_structures_and_algorithms_using_javascript/chapter5/queue.js
@@ -0,0 +1,59 @@
+/*************** 队列的实现 *************/
+function Queue() {
+    this.dataStore = [];
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.front = front;
+    this.back = back;
+    this.toString = toString;
+    this.empty = empty;
+}
+
+// 向队尾添加一个元素
+function enqueue(element) {
+    this.dataStore.push(element);
+}
+
+// 删除队首元素
+function dequeue() {
+    return this.dataStore.shift();
+}
+
+// 读队首和队尾的元素
+function front() {
+    return this.dataStore[0];
+}
+
+function back() {
+    return this.dataStore[this.dataStore.length-1];
+}
+
+// 显示队列内所有元素
+function toString() {
+    var retStr = "";
+    for (let i = 0; i < this.dataStore.length; ++i) {
+        retStr += this.dataStore[i] + "\n";
+    }
+    return retStr;
+}
+
+// 判断队列是否为空
+function empty() {
+    if (this.dataStore.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+/********************************** 队列的测试 *************************************/
+var q = new Queue();
+q.enqueue("Meredith");
+q.enqueue("Cynthia");
+q.enqueue("Jennifer");
+console.log(q.toString());
+q.dequeue();
+console.log(q.toString());
+console.log("Front of queue: " + q.front());
+console.log("Back of queue: " + q.back());
