750c572e54e9c7f80ec0597ceb559b78ae26ba9c
xjr7670 <xjr30226@126.com>
2019-02-15 21:22:55 +0800

    Dictionary implement

--- data_structures_and_algorithms_using_javascript/chapter7/Dictionary.js
+++ data_structures_and_algorithms_using_javascript/chapter7/Dictionary.js
@@ -5,6 +5,8 @@ function Dictionary() {
     this.find = find;
     this.remove = remove;
     this.showAll = showAll;
+    this.count = count;
+    this.clear = clear;
 }
 
 function add(key, value) {
@@ -25,11 +27,30 @@ function showAll() {
     }
 }
 
+// 获取字典元素个数
+function count() {
+    var n = 0;
+    for (let key in this.dataStore) {
+        ++n;
+    }
+    return n;
+}
+
+// 清空字典
+function clear() {
+    for (let key in this.dataStore) {
+        delete this.dataStore[key];
+    }
+}
+
 /***************** 使用 Dictionary 类 *****************/
 var pbook = new Dictionary()
 pbook.add("Mike", "123");
 pbook.add("David", "345");
 pbook.add("Cynthia", "456");
+console.log("Number of entries: " + pbook.count());
 console.log("David's extension: " + pbook.find("David"));
-pbook.remove("David");
 pbook.showAll();
+pbook.remove("Mike");
+pbook.clear();
+console.log("Number of entries: " + pbook.count());
