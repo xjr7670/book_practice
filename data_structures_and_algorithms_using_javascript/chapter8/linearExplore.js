27c7b2cd5edb71c6425d2ad20a19982e4b44866f
xjr7670 <xjr30226@126.com>
2019-02-16 11:57:05 +0800

    线性探测法

---
+++ data_structures_and_algorithms_using_javascript/chapter8/linearExplore.js
@@ -0,0 +1,87 @@
+// ********** 碰撞冲突解决 － 结尾探测法
+// 散列表
+function HashTable() {
+    this.table = new Array(137);
+    this.simpleHash = simpleHash;
+    this.betterHash = betterHash;
+    this.showDistro = showDistro;
+    this.buildChains = buildChains;
+    this.values = [];
+    this.put = put;
+    this.get = get;
+}
+
+// 散列函数
+function simpleHash(data) {
+    var total = 0;
+    for (let i = 0; i < data.length; ++i) {
+        total += data.charCodeAt(i);   
+    }
+    return total % this.table.length;
+}
+
+// 更好的散列函数
+function betterHash(string) {
+    const H = 37;
+    var total = 0;
+    for (let i = 0; i < string.length; ++i) {
+        total += H * total + string.charCodeAt(i);
+    }
+    total = total % this.table.length;
+    if (total < 0) {
+        total += this.table.length - 1;
+    }
+    return parseInt(total);
+}
+
+function put(key, data) {
+    var pos = this.betterHash(key);
+    if (this.table[pos] == undefined) {
+        this.table[pos] = key;
+        this.values[pos] = data;
+    } else {
+        while (this.table[pos] != undefined) {
+            pos++;
+        }
+        this.table[pos] = key;
+        this.values[pos] = data;
+    }    
+}
+
+function get(key) {
+    var hash = -1;
+    hash = this.betterHash(key);
+    if (hash > -1) {
+        for (let i = hash; this.table[hash] != undefined; i++) {
+            if (this.table[hash] == key) {
+                return this.values[hash];
+            }
+        }
+    }
+    return undefined;
+}
+
+function showDistro() {
+    var n = 0;
+    for (let i = 0; i < this.table.length; ++i) {
+        if (this.table[i][0] != undefined) {
+            console.log(i + ": " + this.table[i]);
+        }
+    }
+}
+
+// 开链法
+function buildChains() {
+    for (let i = 0; i < this.table.length; ++i) {
+        this.table[i] = new Array();
+    }
+}
+
+/******************* 使用开链法避免碰撞 *************************/
+var hTable = new HashTable();
+hTable.buildChains();
+var someNames = ["David", "Jennifer", "Donnie", "Raymond", "Cynthia", "Mike", "Clayton", "Danny", "Jonathan"];
+for (let i = 0; i < someNames.length; ++i) {
+    hTable.put(someNames[i]);
+}
+hTable.showDistro();
