c3e8719650f1bc46c761a1a05f8d2ec0132c9e60
xjr7670 <xjr30226@126.com>
2019-02-17 18:26:52 +0800

    快速排序

---
+++ data_structures_and_algorithms_using_javascript/chapter12/quickSort.js
@@ -0,0 +1,225 @@
+// 快速排序
+
+// 数组测试平台
+function CArray(numElements) {
+    this.dataStore = [];
+    this.pos = 0;
+    this.numElements = numElements;
+    this.insert = insert;
+    this.toString = toString;
+    this.clear = clear;
+    this.setData = setData;
+    this.swap = swap;
+    for (let i = 0; i < numElements; ++i) {
+        this.dataStore[i] = i;
+    }
+    this.bubbleSort = bubbleSort;
+    this.selectionSort = selectionSort;
+    this.insertionSort = insertionSort;
+
+    // 希尔排序
+    this.gaps = [5, 3, 1];
+    this.shellSort = shellSort;
+    this.shellSort1 = shellSort1;
+    
+    // 归并排序
+    this.mergeSort = mergeSort;
+    this.mergeArrays = mergeArrays;
+    for (let i = 0; i < numElements; ++i) {
+        this.dataStore[i] = 0;
+    }
+
+    // 快速排序
+    this.qSort = qSort;
+}
+
+function setData() {
+    for (let i = 0; i < this.numElements; ++i) {
+        this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
+    }
+}
+
+function clear() {
+    for (let i = 0; i < this.dataStore.length; ++i) {
+        this.dataStore[i] = 0;
+    }
+}
+
+function insert(element) {
+    this.dataStore[this.pos++] = element;
+}
+
+function toString() {
+    var retstr = "";
+    for (let i = 0; i < this.dataStore.length; ++i) {
+        retstr += this.dataStore[i] + " ";
+        if (i > 0 && i % 10 == 0) {
+            retstr += "\n";
+        }
+    }
+    return retstr;
+}
+
+function swap(arr, index1, index2) {
+    var temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+
+// 冒泡排序算法
+function bubbleSort() {
+    var numElements = this.dataStore.length;
+    var temp;
+    for (let outer = numElements; outer >= 2; --outer) {
+        for (var inner = 0; inner <= outer - 1; ++inner) {
+            if (this.dataStore[inner] > this.dataStore[inner + 1]) {
+                swap(this.dataStore, inner, inner + 1);
+            }
+        }
+        // console.log(this.toString());
+    }
+}
+
+// 选择排序算法
+function selectionSort() {
+    var min, temp;
+    for (let outer = 0; outer <= this.dataStore.length - 2; ++outer) {
+        min = outer;
+        for (let inner = outer + 1; inner <= this.dataStore.length - 1; ++inner) {
+            if (this.dataStore[inner] < this.dataStore[min]) {
+                min = inner;
+            }
+            swap(this.dataStore, outer, min);
+        }
+        // console.log(this.dataStore.toString());
+    }
+}
+
+// 插入排序算法
+function insertionSort() {
+    var temp, inner;
+    for (let outer = 1; outer <= this.dataStore.length - 1; ++outer) {
+        temp = this.dataStore[outer];
+        inner = outer;
+        while (inner > 0 && (this.dataStore[inner - 1] >= temp)) {
+            this.dataStore[inner] = this.dataStore[inner - 1];
+            --inner;
+        }
+        this.dataStore[inner] = temp;
+    }
+}
+
+// 希尔排序
+function shellSort() {
+    for (let g = 0; g < this.gaps.length; ++g) {
+        for (let i = this.gaps[g]; i < this.dataStore.length; ++i) {
+            var temp = this.dataStore[i];
+            for (var j = i; j >= this.gaps[g] && this.dataStore[j-this.gaps[g]] > temp; j -= this.gaps[g]) {
+                this.dataStore[j] = this.dataStore[j-this.gaps[g]];
+                
+            }
+            this.dataStore[j] = temp;
+        }
+       // console.log(this.dataStore.toString());
+    }
+}
+
+// 动态计算间隔序列的希尔排序
+function shellSort1() {
+    var N = this.dataStore.length;
+    var h = 1;
+    while (h < N/3) {
+        h = 3 * h + 1;
+    }
+    while (h >= 1) {
+        for (let i = h; i < N; i++) {
+            for (let j = i; j >= h && this.dataStore[j] < this.dataStore[j-h]; j -= h) {
+                swap(this.dataStore, j, j-h);
+            }
+        }
+        h = (h-1) / 3;
+    }
+}
+
+function setGaps(arr) {
+    this.gaps = arr;
+}
+
+// 归并排序
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return;
+    }
+    var step = 1;
+    var left, right;
+    while (step < arr.length) {
+        left = 0;
+        right = step;
+        while (right + step <= arr.length) {
+            mergeArrays(arr, left, left+step, right, right+step);
+            left = right + step;
+            right = left + step;
+        }
+        if (right < arr.length) {
+            mergeArrays(arr, left, left+step, right, arr.length);
+        }
+        step *= 2;
+    }
+}
+
+function mergeArrays(arr, startLeft, stopLeft, startRight, stopRight) {
+    var rightArr = new Array(stopRight - startRight + 1);
+    var leftArr = new Array(stopLeft - startLeft + 1);
+    k = startRight;
+    for (let i = 0; i < (rightArr.length - 1); ++i) {
+        rightArr[i] = arr[k];
+        ++k;
+    }
+    k = startLeft;
+    for (let i = 0; i < (leftArr.length - 1); ++i) {
+        leftArr[i] = arr[k];
+        ++k;
+    }
+    rightArr[rightArr.length-1] = Infinity;
+    leftArr[leftArr.length-1] = Infinity;
+    var m = 0;
+    var n = 0;
+    for (let k = startLeft; k < stopRight; ++k) {
+        if (leftArr[m] <= rightArr[n]) {
+            arr[k] = leftArr[m];
+            m++;
+        } else {
+            arr[k] = rightArr[n];
+            n++;
+        }
+    }
+    console.log("left array - ", leftArr);
+    console.log("right array - ", rightArr);
+}
+
+// 快速排序
+function qSort(list) {
+    if (list.length == 0) {
+        return [];
+    }
+    var lesser = [];
+    var greater = [];
+    var pivot = list[0];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i] < pivot) {
+            lesser.push(list[i]);
+        } else {
+            greater.push(list[i]);
+        }
+    }
+    return qSort(lesser).concat(pivot, qSort(greater));
+}
+
+var a = [];
+for (let i = 0; i < 10; i++) {
+    a[i] = Math.floor((Math.random() * 100)+1);
+}
+console.log(a);
+console.log();
+console.log(qSort(a));
