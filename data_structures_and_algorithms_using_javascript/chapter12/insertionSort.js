e39c97230a6473c0aabbe655e1a85ffc7305cbc2
xjr7670 <xjr30226@126.com>
2019-02-17 16:23:27 +0800

    insertion Sort

---
+++ data_structures_and_algorithms_using_javascript/chapter12/insertionSort.js
@@ -0,0 +1,75 @@
+// 插入排序
+
+// 插入排序
+// 冒泡排序
+// 数组测试平台
+function CArray(numElements) {
+    this.dataStore = [];
+    this.pos = 0;
+    this.numElements = numElements;
+    this.insert = insert;
+    this.toString = toString;
+    this.clear = clear;
+    this.setData = setData;
+    this.swap = swap;
+    for (let i = 0; i < numElements; ++i) {
+        this.dataStore[i] = i;
+    }
+    this.insertionSort = insertionSort;
+}
+
+function setData() {
+    for (let i = 0; i < this.numElements; ++i) {
+        this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
+    }
+}
+
+function clear() {
+    for (let i = 0; i < this.dataStore.length; ++i) {
+        this.dataStore[i] = 0;
+    }
+}
+
+function insert(element) {
+    this.dataStore[this.pos++] = element;
+}
+
+function toString() {
+    var retstr = "";
+    for (let i = 0; i < this.dataStore.length; ++i) {
+        retstr += this.dataStore[i] + " ";
+        if (i > 0 && i % 10 == 0) {
+            retstr += "\n";
+        }
+    }
+    return retstr;
+}
+
+function swap(arr, index1, index2) {
+    var temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
+// 插入排序算法
+function insertionSort() {
+    var temp, inner;
+    for (let outer = 1; outer <= this.dataStore.length - 1; ++outer) {
+        temp = this.dataStore[outer];
+        inner = outer;
+        while (inner > 0 && (this.dataStore[inner - 1] >= temp)) {
+            this.dataStore[inner] = this.dataStore[inner - 1];
+            --inner;
+        }
+        this.dataStore[inner] = temp;
+    }
+}
+
+// 插入排序
+var numElements = 10;
+var mynums = new CArray(numElements);
+mynums.setData();
+console.log(mynums.toString());
+mynums.insertionSort();
+console.log()
+console.log(mynums.toString());
