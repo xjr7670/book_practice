05b6a76d2e8551a60d726b1f32b5f8f50797fb01
xjr7670 <xjr30226@126.com>
2019-03-02 16:17:09 +0800

    pool puzzle one, not finish

---
+++ HeadFirstJAVA/chapter1/PoolPuzzleOne.java
@@ -0,0 +1,25 @@
+class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 4) {
+            System.out.print("a");
+            if (x < 1) {
+                System.out.print(" ");
+            }
+            System.out.print("noise");
+            if () {
+                ;
+                ;
+            }
+            if (x == 1) {
+                System.out.print("annoys");
+            }
+            if () {
+
+            }
+            System.out.println("");
+            x = x + 1;
+        }
+    }
+}
