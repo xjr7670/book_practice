337043b7950cbe84fc260395b1eb39ff89001397
xjr7670 <xjr30226@126.com>
2019-03-12 21:50:08 +0800

    practice

---
+++ HeadFirstJAVA/chapter4/ElectricGuitar.java
@@ -0,0 +1,29 @@
+class ElectricGuitar {
+    String brand;
+    int numOfPickups;
+    boolean rockStarUsesIt;
+
+    String getBrand() {
+        return brand;
+    }
+
+    void setBrand(String aBrand) {
+        brand = aBrand;
+    }
+
+    void getNumOfPickups() {
+        return numOfPickups;
+    }
+
+    void setNumOfPickups(int num) {
+        numOfPickups = num;
+    }
+
+    boolean getRockStarUsesIt() {
+        return rockStarUsesIt;
+    }
+
+    void setRockStarUsesIt(boolean yesOrNo) {
+        rockStarUsesIt = yesOrNo;
+    }
+}
