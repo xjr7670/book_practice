19127a0a93276e6d815bd7fc9f7fbeec1211ec65
xjr7670 <xjr30226@126.com>
2019-03-31 17:51:43 +0800

    10进制数转2进制数，Python动态规划实现

---
+++ HeadFirstJAVA/chapter7/Dec2Bin_v2.py
@@ -0,0 +1,36 @@
+#-*- coding:utf-8 -*-
+
+'''
+动态规划法实现
+'''
+
+class Dec2Bin(object):
+
+    def __init__(self):
+        self.number = 18719460345
+        self.result_list = []
+    
+    def get_rest(self, num):
+        
+        quotient = num // 2
+        remainder = num % 2
+
+        return quotient, remainder
+    
+    def main(self):
+
+        quotient = self.number
+        while quotient >= 1:
+            quotient, remainder = self.get_rest(quotient)
+            self.result_list.append(remainder)
+
+        for i in self.result_list[::-1]:
+            print(i, end='')
+        else:
+            print()         
+
+
+if __name__ == '__main__':
+
+    dec2bin = Dec2Bin()
+    dec2bin.main()
