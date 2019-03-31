174ff7fe1485b4b99231f590554577b0ceb2d43a
root <root@Cavin-Y7000.localdomain>
2019-03-31 17:49:14 +0800

    10进制转2进制，java递归实现

---
+++ HeadFirstJAVA/chapter7/Dec2Bin.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Dec2Bin {
+    public static void main(String[] args) {
+        ArrayList<Integer> binList = new ArrayList<Integer>();
+        long number = 18719460345L;
+        GetBin(number, binList);
+        for (int i = binList.size()-1; i >= 0; i--) {
+            System.out.print(binList.get(i));
+        }
+    }
+
+    public static long GetBin(long number, ArrayList<Integer> list) {
+        int rest = 0;
+        long shang = 0L;
+        rest = (int) (number % 2);
+        shang = (long) (number / 2);
+        list.add(rest);
+        if (shang <= 1) {
+            list.add((int) shang);
+            return shang;
+        } else {
+            return GetBin(shang, list);
+        }
+    }
+}
