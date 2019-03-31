7366ee00afe1c1e394c8341ea9cd16f1115e293f
xjr7670 <xjr30226@126.com>
2019-03-31 17:51:31 +0800

    10进制数转2进制数，Python递归实现

---
+++ HeadFirstJAVA/chapter7/Dec2Bin.py
@@ -0,0 +1,37 @@
+#-*- coding:utf-8 -*-
+
+'''
+递归法实现
+'''
+
+class Dec2Bin(object):
+
+    def __init__(self):
+        self.number = 18719460345
+        self.result_list = []
+    
+    def get_rest(self, num):
+
+        remainder = num % 2
+        quotient = num // 2
+        self.result_list.append(remainder)
+
+        if quotient <= 1:
+            self.result_list.append(quotient)
+            return quotient
+        else:
+            return self.get_rest(quotient)
+    
+    def main(self):
+
+        self.get_rest(self.number)
+        for i in self.result_list[::-1]:
+            print(i, end='')
+        else:
+            print()
+
+
+if __name__ == '__main__':
+
+    dec2bin = Dec2Bin()
+    dec2bin.main()
