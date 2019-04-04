978aa252824463393fab4555050f4abab1ae42af
xjr7670 <xjr30226@126.com>
2019-04-04 17:20:15 +0800

    基类 Animal

---
+++ HeadFirstJAVA/chapter9/Animal.java
@@ -0,0 +1,10 @@
+public abstract class Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public Animal(String theName) {
+        name = theName;
+    }
+}
