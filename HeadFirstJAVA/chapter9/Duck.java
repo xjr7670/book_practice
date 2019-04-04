d319aac5078d0d1685fa27cfec896c0e941e6a45
xjr7670 <xjr30226@126.com>
2019-04-04 15:50:42 +0800

    构造器与垃圾收集器

---
+++ HeadFirstJAVA/chapter9/Duck.java
@@ -0,0 +1,9 @@
+public class Duck {
+    int size;
+
+    public Duck(int duckSize) {
+        System.out.println("Quack");
+        size = duckSize;
+        System.out.println("size is " + size);
+    }
+}
---
+++ HeadFirstJAVA/chapter9/UseDuck.java
@@ -0,0 +1,5 @@
+public class UseDuck {
+    public static void main(String[] args) {
+        Duck d = new Duck(42);
+    }
+}
