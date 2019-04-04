1df2e4aa204e4ebdd84ca796f366c2baebd5b32b
xjr7670 <xjr30226@126.com>
2019-04-04 16:00:15 +0800

    重载过的构造函数

---
+++ HeadFirstJAVA/chapter9/TestDuck.java
@@ -0,0 +1,19 @@
+public class TestDuck {
+    public static void main(String[] args) {
+        int weight = 8;
+        float density = 2.3F;
+        String name = "Donald";
+        long[] feathers = {1, 2, 3, 4, 5, 6};
+        boolean canFly = true;
+        int airspeed = 22;
+
+        Duck[] d = new Duck[7];
+        d[0] = new Duck();
+        d[1] = new Duck(density, weight);
+        d[2] = new Duck(name, feathers);
+        d[3] = new Duck(canFly);
+        d[4] = new Duck(3.3F, airspeed);
+        d[5] = new Duck(false);
+        d[6] = new Duck(airspeed, density);
+    }
+}
