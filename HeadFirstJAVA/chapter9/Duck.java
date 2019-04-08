806426489dfd93ac7e0e7775a48383384803bcc1
xjr7670 <xjr30226@126.com>
2019-04-08 11:38:45 +0800

    no change

--- HeadFirstJAVA/chapter9/Duck.java
+++ HeadFirstJAVA/chapter9/Duck.java
@@ -1,9 +1,35 @@
-public class Duck {
-    int size;
+class Duck {
+    int pounds = 6;
+    float floatability = 2.1F;
+    String name = "Generic";
+    long[] feathers = {1, 2, 3, 4, 5, 6, 7};
+    boolean canFly = true;
+    int maxSpeed = 25;
 
-    public Duck(int duckSize) {
-        System.out.println("Quack");
-        size = duckSize;
-        System.out.println("size is " + size);
+    public Duck() {
+        System.out.println("type 1 duck");
+    }
+
+    public Duck(boolean fly) {
+        canFly = fly;
+        System.out.println("type 2 duck");
+    }
+
+    public Duck(String n, long[] f) {
+        name = n;
+        feathers = f;
+        System.out.println("type 3 duck");
+    }
+
+    public Duck(int w, float f) {
+        pounds = w;
+        floatability = f;
+        System.out.println("type 4 duck");
+    }
+
+    public Duck(float density, int max) {
+        floatability = density;
+        maxSpeed = max;
+        System.out.println("type 5 duck");
     }
 }
