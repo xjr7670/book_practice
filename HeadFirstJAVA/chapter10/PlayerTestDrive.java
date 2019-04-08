2b18b4449d7ba37f7a3f6f2ed424db7b6d1fd4bc
xjr7670 <xjr30226@126.com>
2019-04-08 11:37:37 +0800

    实例变量

---
+++ HeadFirstJAVA/chapter10/PlayerTestDrive.java
@@ -0,0 +1,18 @@
+class Player {
+    static int playerCount = 0;
+    private String name;
+    public Player(String n) {
+        name = n;
+        playerCount++;
+    }
+}
+
+public class PlayerTestDrive {
+    public static void main(String[] args) {
+        System.out.println(Player.playerCount);
+        Player one = new Player("Tiger Woods");
+        System.out.println(Player.playerCount);
+        Player tow = new Player("Loyer Chen");
+        System.out.println(Player.playerCount);
+    }
+}
