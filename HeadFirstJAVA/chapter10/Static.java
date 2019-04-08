48e9981defadb4d4c0ef4e47f35906ecf29763cd
xjr7670 <xjr30226@126.com>
2019-04-08 11:38:33 +0800

    see static variable

--- HeadFirstJAVA/chapter10/Static.java
+++ HeadFirstJAVA/chapter10/Static.java
@@ -1,6 +1,11 @@
 public class Static {
     static int size = 10;
     public static void main(String[] args) {
+        Static s = new Static();
+        s.func();
+    }
+
+    public void func() {
         System.out.println(size);
     }
 }
