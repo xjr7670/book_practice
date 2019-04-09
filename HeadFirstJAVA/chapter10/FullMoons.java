16d6d31788ad69178b809979987a4738a3e8665a
xjr7670 <xjr30226@126.com>
2019-04-09 11:55:42 +0800

    计算满月日期

---
+++ HeadFirstJAVA/chapter10/FullMoons.java
@@ -0,0 +1,16 @@
+import java.util.Calendar;
+import static java.lang.System.out;
+
+class FullMoons {
+    static int DAY_IM = 1000 * 60 * 60 * 24;
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        c.set(2019, 3, 9, 11, 54);
+        long day1 = c.getTimeInMillis();
+        for (int x = 0; x < 60; x++) {
+            day1 += (DAY_IM * 29.52);
+            c.setTimeInMillis(day1);
+            out.println(String.format("full moon on %tc", c));
+        }
+    }
+}
