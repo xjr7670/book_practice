96d0236056d3506d5ad0f520c63c35280693f3cb
xjr7670 <xjr30226@126.com>
2019-04-17 15:25:13 +0800

    简单的GUI3C

---
+++ HeadFirstJAVA/chapter12/SimpleGui3C.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SimpleGui3C implements ActionListener {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SimpleGui3C gui = new SimpleGui3C();
+        gui.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Change colors");
+        button.addActionListener(this);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        frame.repaint();
+    }
+}
+
+class MyDrawPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        Color startColor = new Color(red, green, blue);
+
+        red = (int) (Math.random() * 255);
+        green = (int) (Math.random() * 255);
+        blue = (int) (Math.random() * 255);
+        Color endColor = new Color(red, green, blue);
+        
+        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100);
+    }
+}
