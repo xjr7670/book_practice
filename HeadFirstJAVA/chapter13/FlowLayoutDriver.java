b33d7e0f1222f8d2f4d88071d89fb424c458f850
xjr7670 <xjr30226@126.com>
2019-04-18 12:32:46 +0800

    FlowLayoutt布局

--- HeadFirstJAVA/chapter13/FlowLayoutDriver.java
+++ HeadFirstJAVA/chapter13/FlowLayoutDriver.java
@@ -1,8 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class FlowLayoutDriver {l    public static void main(String[] args) {
-        Panel1 gui = new Panel1();
+public class FlowLayoutDriver {
+    public static void main(String[] args) {
+        FlowLayoutDriver gui = new FlowLayoutDriver();
         gui.go();
     }
 
@@ -10,8 +11,15 @@ public class FlowLayoutDriver {l    public static void main(String[] args) {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         panel.setBackground(Color.darkGray);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JButton button = new JButton("shock me");
+        JButton buttonTwo = new JButton("bliss");
+        JButton buttonThree = new JButton("huh?");
+        panel.add(button);
+        panel.add(buttonTwo);
+        panel.add(buttonThree);
         frame.getContentPane().add(BorderLayout.EAST, panel);
-        frame.setSize(200, 200);
+        frame.setSize(250, 200);
         frame.setVisible(true);
     }
 }
