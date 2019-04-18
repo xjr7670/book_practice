3f34735f3447cde56e93390e4217045973cfdcde
xjr7670 <xjr30226@126.com>
2019-04-18 12:27:11 +0800

    FlowLayoutt布局

---
+++ HeadFirstJAVA/chapter13/FlowLayoutDriver.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class FlowLayoutDriver {l    public static void main(String[] args) {
+        Panel1 gui = new Panel1();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+    }
+}
