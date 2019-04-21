82ad3d9294342dd81b9edf99f651ff6f3de2d279
xjr7670 <xjr30226@126.com>
2019-04-21 11:27:04 +0800

    set and get quiz

---
+++ HeadFirstJAVA/chapter14/QuizCard.java
@@ -0,0 +1,16 @@
+public class QuizCard {
+    public String question;
+    public String answer;
+    public QuizCard(String q, String a) {
+        question = q;
+        answer = a;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
