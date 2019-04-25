168738dd5765a2b07d438b763a191238a9ba67be
xjr7670 <xjr30226@126.com>
2019-04-25 23:07:40 +0800

    可比较的

---
+++ HeadFirstJAVA/chapter16/Jukebox3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Jukebox3 {
+    ArrayList<Song> songList = new ArrayList<Song>();
+
+    public static void main(String[] args) {
+        Jukebox1 jb = new Jukebox1();
+        jb.go();
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+
+    // 读取文件
+    void getSongs() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("SongList.txt"), "UTF-8"));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+        songList.add(nextSong);
+    }
+}
