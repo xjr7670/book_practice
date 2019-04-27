9cbf58ebbfb382ef91d0777186fad9ec08e2cff7
xjr7670 <xjr30226@126.com>
2019-04-27 22:54:03 +0800

    add some method

--- HeadFirstJAVA/chapter16/Song.java
+++ HeadFirstJAVA/chapter16/Song.java
@@ -4,6 +4,15 @@ class Song implements Comparable<Song> {
     String rating;
     String bpm;
 
+    public boolean equals(Object aSong) {
+        Song s = (Song) aSong;
+        return getTitle().equals(s.getTitle());
+    }
+
+    public int hashCode() {
+        return title.hashCode();
+    }
+
     public int compareTo(Song s) {
         return title.compareTo(s.getTitle());
     }
