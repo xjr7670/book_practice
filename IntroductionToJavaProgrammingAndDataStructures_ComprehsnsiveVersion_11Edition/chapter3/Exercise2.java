200151023fd499bbcbc7c18c939419479862ac6f
xjr7670 <xjr30226@126.com>
2019-06-23 19:36:28 +0800

    猜 3 数之和

---
+++ IntroductionToJavaProgrammingAndDataStructures_ComprehsnsiveVersion_11Edition/chapter3/Exercise2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int number1 = (int) (System.currentTimeMillis() % 10);
+        int number2 = (int) (System.currentTimeMillis() / 10 % 10);
+        int number3 = (int) (System.currentTimeMillis() / 100 % 10);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is " + number1 + " + " + number2 + " + " + number3 + "? ");
+
+        int answer = input.nextInt();
+
+        System.out.println(number1 + " + " + number2 + " + " + number3 + " = " + answer + " is " + (number1 + number2 + number3 == answer));
+    }
+}
