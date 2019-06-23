82b8b2adec77a3eaae6e245c56cab10022611bd8
xjr7670 <xjr30226@126.com>
2019-06-23 17:02:09 +0800

    计算应交税

---
+++ IntroductionToJavaProgrammingAndDataStructures_ComprehsnsiveVersion_11Edition/chapter3/ComputeTax.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ComputeTax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("(0-single filter, 1-married jointly or " +
+            "qualifying window(er), 2-married separately, 3-head of " +
+            "household) Enter the filing status: ");
+        int status = input.nextInt();
+
+        System.out.print("Enter the taxable incomes: ");
+        double income = input.nextDouble();
+
+        double tax = 0;
+
+        if (status == 0) {
+            if (income <= 8350) {
+                tax = income * 0.10;
+            } else if (income <= 33950) {
+                tax = 8350 * 0.10 + (income - 8350) * 0.15;
+            } else if (income <= 82250) {
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (income - 33950) * 0.25;
+            } else if (income <= 171550) {
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (income - 82250) * 0.28;
+            } else if (income <= 372950) {
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171500 - 82250) * 0.28 + (income - 171550) * 0.33;
+            } else {
+                tax = 8350 * 0.10 + (33950 - 8350) * 0.15 + (82250 - 33950) * 0.25 + (171500 - 82250) * 0.28 + (372950 - 171550) * 0.33 + (income - 372950) * 0.35;
+            }
+        } else if (status == 1) {
+            // todo...
+        } else if (status == 2) {
+            // todo...
+        } else if (status == 3) {
+            // todo...
+        } else {
+            System.out.println("Error: invalid status.");
+            System.exit(1);
+        }
+
+        System.out.println("Tax is " + (int) (tax * 100) / 100.0);
+    }
+}
