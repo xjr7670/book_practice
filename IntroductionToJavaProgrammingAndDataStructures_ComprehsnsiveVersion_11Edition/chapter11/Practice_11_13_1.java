bedd8c7d64abe5c35e78c311bdd88306c9a90956
xjr7670 <xjr30226@126.com>
2019-07-20 15:45:41 +0800

    自定义栈类及其测试用类

---
+++ IntroductionToJavaProgrammingAndDataStructures_ComprehsnsiveVersion_11Edition/chapter11/Practice_11_13_1.java
@@ -0,0 +1,7 @@
+public class Practice_11_13_1 {
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        myStack.push(11);
+        System.out.println(myStack);
+    }
+}
