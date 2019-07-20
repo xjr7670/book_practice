3f76f09b48b5b1b54c0f536eadbb40194cb866dc
xjr7670 <xjr30226@126.com>
2019-07-20 11:13:05 +0800

    使用数组列表来获取唯一元素的列表

---
+++ IntroductionToJavaProgrammingAndDataStructures_ComprehsnsiveVersion_11Edition/chapter11/DistinctNumbers.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DistinctNumbers {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter integers (input ends with 0): ");
+        int value;
+
+        do {
+            value = input.nextInt();
+
+            if (!list.contains(value) && value != 0) {
+                list.add(value);
+            }
+        } while (value != 0);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
