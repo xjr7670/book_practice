e4b9d5caa65b6495396420670c860de8ca0f8385
xjr7670 <xjr30226@126.com>
2019-07-25 23:13:48 +0800

    几何图片的测试类

---
+++ IntroductionToJavaProgrammingAndDataStructures_ComprehsnsiveVersion_11Edition/chapter13/TestGeometricObject.java
@@ -0,0 +1,25 @@
+public class TestGeometricObject {
+    public static void main(String[] args) {
+        // Create two geometric objects
+        GeometricObject geoObject1 = new Circle(5);
+        GeometricObject geoObject2 = new Rectangle(5, 3);
+
+        System.out.println("The two objects have the same area? " + equalArea(geoObject1, geoObject2));
+
+        // Display circle
+        displayGeometricObject(geoObject1);
+
+        // Display Rectangle
+        displayGeometricObject(geoObject2);
+    }
+
+    public static boolean equalArea(GeometricObject object1, GeometricObject object2) {
+        return object1.getArea() == object2.getArea();
+    }
+
+    public static void displayGeometricObject(GeometricObject object) {
+        System.out.println();
+        System.out.println("The area is " + object.getArea());
+        System.out.println("The perimeter is " + object.getPerimeter());
+    }
+}
