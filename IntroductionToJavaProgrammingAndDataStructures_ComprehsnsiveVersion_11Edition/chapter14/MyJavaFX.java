332e4b0c60705af0c7dfbb74269467de22ff0e4d
xjr7670 <xjr30226@126.com>
2019-07-29 21:31:57 +0800

    chapter14 javaFX基础

---
+++ IntroductionToJavaProgrammingAndDataStructures_ComprehsnsiveVersion_11Edition/chapter14/MyJavaFX.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MyJavaFX extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btOK = new Button("OK");
+        Scene scene = new Scene(btOK, 200, 250);
+        primaryStage.setTitle("MyJavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
