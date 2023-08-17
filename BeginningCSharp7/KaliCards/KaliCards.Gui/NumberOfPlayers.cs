c5d6504f2777481ea6688429f01cc45ba6b3e79b
xjr7670 <xjr30226@126.com>
2023-08-17 18:08:31 +0800

    add two

---
+++ BeginningCSharp7/KaliCards/KaliCards.Gui/NumberOfPlayers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaliCards.Gui
+{
+    public class NumberOfPlayers : ObservableCollection<int>
+    {
+        public NumberOfPlayers() : base()
+        {
+            Add(2);
+            Add(3);
+            Add(4);
+        }
+    }
+}
